f47d9cf8c2082ad7a433dc035ad82f56d2dea3e0
JameGran <tamef.granados@gmail.com>
2024-05-31 08:17:47 -0500

    fix deploy

--- piccolas/src/environments/configuracion/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    base_url: "https://piccolas-backend.onrender.com/api/v1",
-
-}
---
+++ piccolas/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    base_url: "https://pegasus-backend-wiax.onrender.com/api/v1",
+
+}
